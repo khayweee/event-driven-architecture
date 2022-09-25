a1b0e19f3a56c11ad4c5849c82c1a5f31c2c92c8
khaywee <khayweeang@gmail.com>
2022-09-25 22:02:39 +0800

    Rebuilding state when redis fail

--- backend/main.py
+++ backend/main.py
@@ -47,7 +47,27 @@ async def get_state(pk: str):
     
     if state is not None:
         return json.loads(state)
-    return {}
+    
+    state = build_state(pk)
+    redis.set(f'delivery:{pk}', json.dumps(state))
+    return state
+
+def build_state(pk: str):
+    """
+    To be used when database of event failed
+    Chains of events can be rebuilt from event logs
+    TODO: To order all events by creation time
+    """
+    pks = Event.all_pks()
+    all_events = [Event.get(pk) for pk in pks]
+    events = [event for event in all_events if event.delivery_id == pk]
+
+    state = {}
+    # Redo State
+    for event in events:
+        state = consumers.CONSUMERS[event.type](state, event)
+
+    return state
 
 @app.post('/deliveries/create')
 async def create(request: Request):
